8169049f131bb466a5bc51c2be0c244a92f06581
Dmitry <khv1702@gmail.com>
2025-03-21 03:55:11 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
+	publicDir: 'public',
+	server: {
+		port: 3280,
+		strictPort: true
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src')
 		}
 	},
-
 	plugins: [react()]
 });
